7e6798339498b994b2fad5dfd030d8b8e8eef401
TitiLeGrand <thibaultcallerand@gmail.com>
2025-05-12 14:36:53 +0200

    Commençons par l'entité produit (partie 2)

--- src/Product.php
+++ src/Product.php
@@ -1,15 +1,40 @@
 <?php
 // src/Product.php
 use Doctrine\ORM\Mapping as ORM;
+
 #[ORM\Entity]
 #[ORM\Table(name: 'products')]
 class Product
 {
-#[ORM\Id]
-#[ORM\Column(type: 'integer')]
-#[ORM\GeneratedValue]
-private int|null $id = null;
-#[ORM\Column(type: 'string')]
-private string $name;
-// .. (other code)
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    // Constructeur pour initialiser le nom à la création (optionnel)
+    public function __construct(string $name = '')
+    {
+        $this->name = $name;
+    }
+
+    // Getter pour $id
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // Getter pour $name
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    // Setter pour $name
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
